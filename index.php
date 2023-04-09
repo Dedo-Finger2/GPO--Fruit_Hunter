dca292165dcbcba0464ae9240dc49fd812ed6694
Antonio Mauricio <antonioimportant@gmail.com>
2023-04-09 09:58:44 -0300

    index melhor

--- index.php
+++ index.php
@@ -22,30 +22,86 @@
         <link rel="stylesheet" href="/fruit_hunter/assets/dist/css/adminlte.min.css">
         <link rel="stylesheet" href="/fruit_hunter/css/style.css">
         <style>
+            body {
+                /* Remova as propriedades de centralização aqui */
+            }
+
+            .wrapper {
+                display: flex;
+                justify-content: center;
+                margin-left: 15rem;
+                align-items: center;
+                height: 93vh;
+            }
+
+            .sidebar {
+                /* Estilos para a barra lateral do menu */
+            }
+
+            .content {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: row;
+                border: 1px solid #000;
+                padding: 20px;
+            }
+
+            select, .enviar {
+                margin: 20px;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+            .enviar {
+                background-color: #000;
+                color: #fff;
+                border: none;
+                cursor: pointer;
+                width: 150px;
+            }
+
+            .enviar:hover {
+                background-color: gray;
+            }
+
+            select, option {
+                border: 1px solid #000;
+                width: 500px;
+                margin-right: 10px;
+                margin-left: 15px;
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+            }
+
 
         </style>
     </head>
     <body>
         
-        <?php require_once("app/View/menu.php") ?>
-
-        <!-- Envia a fruta para o tratamento -->
-        <form action="app/Controller/form/tratamento.php" method="post">
-            <div class="container">
-                <select class="butao_index" style="width: 200px; padding: 10px; font-size: 20px;" name="fruta">
+            <div class="side-bar">
+                <?php require_once("app/View/menu.php") ?>
             </div>
-                <?php
-                    if ($result->num_rows > 0)
-                    {
-                        while($row = $result->fetch_assoc())
-                        {
-                            echo "<option>".$row['nome']. "</option>";
-                        }
-                    }
-                ?>
-            </select>
-            <button style="padding: 10px; width: 200px; font-size: 20px; margin-left: 30px;" class="butao_index" type="submit" name="novo_dia">Enviar</button>
-        </form>
+            <div class="wrapper">
+                <!-- Envia a fruta para o tratamento -->
+                <form action="app/Controller/form/tratamento.php" method="post">
+                    <div class="content">
+                        <select class="selecionar" name="fruta">
+                            <?php
+                                if ($result->num_rows > 0)
+                                {
+                                    while($row = $result->fetch_assoc())
+                                    {
+                                        echo "<option>".$row['nome']. "</option>";
+                                    }
+                                }
+                            ?>
+                        </select>
+                        <button class="enviar" type="submit" name="novo_dia">Enviar</button>
+                    </div>
+                </form>
+            </div>
     </body>
 </html>
 
