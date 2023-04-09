d442f45fef09945cb51bfb4f7f26bf94e8b13f31
Antonio Mauricio <antonioimportant@gmail.com>
2023-04-09 11:53:18 -0300

    edit fruit done

--- app/View/fruit/createfruit.php
+++ app/View/fruit/createfruit.php
@@ -12,22 +12,84 @@
         <link rel="stylesheet" href="/fruit_hunter/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="/fruit_hunter/assets/dist/css/adminlte.min.css">
-        <link rel="stylesheet" href="/fruit_hunter/css/style.css">
+        <style>
+            body {
+            /* Remova as propriedades de centralização aqui */
+            }
+
+            .wrapper {
+                display: flex;
+                justify-content: center;
+                margin-left: 15rem;
+                align-items: center;
+                height: 93vh;
+            }
+
+            .side-bar {
+                
+            }
+
+            .content {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                border: 1px solid #000;
+                padding: 20px;
+                width: 89rem;
+            }
+
+            h2 {
+                text-align: center;
+                margin-right: 3rem;
+            }
+
+            input.dia, button.enviar {
+                margin: 10px;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+            button.enviar {
+                background-color: #000;
+                color: #fff;
+                border: none;
+                cursor: pointer;
+                width: 150px;
+            }
+
+            button.enviar:hover {
+                background-color: gray;
+            }
+
+            .dia, textarea {
+                border: 1px solid #000;
+                width: 500px;
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+            }
+
+
+        </style>
 </head>
 
-<?php require_once("../menu.php") ?>
-
-<div class="main_container">
-    <form action="../../Controller/form/tratamento.php" method="post">
-        <b>Nome:</b> <br><input type="text" name="new_fruta"><br><br>
-        <b>Raridade:</b> <br><select name="new_raridade">
-            <option style="text-align: center;" >Common</option>
-            <option style="text-align: center;" >Rare</option>
-            <option style="text-align: center;" >Epic</option>
-            <option style="text-align: center;" >Legendary</option>
-            <option style="text-align: center;" >Mythic</option>
-        </select>
-        <br><br><br><br><br><br><br>
-        <button style="padding: 10px; width: 200px; font-size: 20px;" type="submit" name="nova_fruta">Enviar</button>
-    </form>
+<div class="side-bar">
+    <?php require_once("../menu.php") ?>
+</div>
+<div class="wrapper">
+    <div class="content">
+        <form action="../../Controller/form/tratamento.php" method="post">
+            <b>Nome:</b> <br><input type="text" name="new_fruta"><br><br>
+            <b>Raridade:</b> <br><select name="new_raridade">
+                <option style="text-align: center;" >Common</option>
+                <option style="text-align: center;" >Rare</option>
+                <option style="text-align: center;" >Epic</option>
+                <option style="text-align: center;" >Legendary</option>
+                <option style="text-align: center;" >Mythic</option>
+            </select>
+            <br><br><br><br><br><br><br>
+            <button class="enviar" type="submit" name="nova_fruta">Enviar</button>
+        </form>
+    </div>
 </div>
--- app/View/fruit/editfruit.php
+++ app/View/fruit/editfruit.php
@@ -21,18 +21,134 @@
         <link rel="stylesheet" href="/fruit_hunter/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
         <!-- Theme style -->
         <link rel="stylesheet" href="/fruit_hunter/assets/dist/css/adminlte.min.css">
-        <link rel="stylesheet" href="/fruit_hunter/css/style.css">
+        <style>
+            body {
+            /* Remova as propriedades de centralização aqui */
+            }
+
+            .wrapper {
+                display: flex;
+                justify-content: center;
+                margin-left: 15rem;
+                align-items: center;
+                height: 93vh;
+            }
+
+            .side-bar {
+                
+            }
+
+            .content {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                border: 1px solid #000;
+                padding: 20px;
+                width: 69rem;
+            }
+
+            h2 {
+                text-align: center;
+                margin-bottom: 20px;
+            }
+
+            .form {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                margin-left: 30px;
+                text-align: center;
+            }
+
+            input.dia, button.enviar {
+                margin: 10px;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+            button.enviar {
+                background-color: #000;
+                color: #fff;
+                border: none;
+                cursor: pointer;
+                width: 150px;
+
+            }
+
+            button.enviar:hover {
+                background-color: gray;
+            }
+
+            .dia, textarea {
+                border: 1px solid #000;
+                width: 500px;
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+            }
+            
+            b.raridade-mythic {
+                background: linear-gradient(to right, #9d7fd3, #3c8dbc 25%, #ffffff 150%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+
+            b.raridade-leg {
+                background: linear-gradient(to right, #FF0000, #FF7F00, #FFFF00, #00FF00, #0000FF, #8B00FF);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+
+            b.raridade-epic {
+                background: linear-gradient(to right, #6c3483 20%, #9b59b6 65%, #ffffff 150%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+
+            b.raridade-rare {
+                background: linear-gradient(to right, #17a2b8, #38b8cc 65%, #ffffff 155%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+            b.raridade-common {
+                background: linear-gradient(to right, #657383, #8695a8 65%, #ffffff 150%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+
+        </style>
 </head>
 
-<?php require_once("../menu.php") ?>
+<div class="side-bar">
+    <?php require_once("../menu.php") ?>
+</div>
+<div class="wrapper">
+    <div class="content">
+        <h2>EDITANDO A FRUTA: <b><?= $row['nome'] ?></b> -
+            <?php 
+                if ($row['raridade'] == "Mythic") {
+                    echo "<b class='raridade-mythic'>". $row['raridade']."</b>";
+                } elseif ($row['raridade'] == "Legendary") {
+                    echo "<b class='raridade-leg'>". $row['raridade']."</b>";
+                } elseif ($row['raridade'] == "Epic") {
+                    echo "<b class='raridade-epic'>". $row['raridade']."</b>";
+                } elseif ($row['raridade'] == "Rare") {
+                    echo "<b class='raridade-rare'>". $row['raridade']."</b>";
+                } elseif ($row['raridade'] == "Common") {
+                    echo "<b class='raridade-common'>". $row['raridade']."</b>";
+                }
 
-<div class="main_container">
-    <form action="../../Controller/form/tratamento.php" method="post">
-        <h2> VOCÊ ESTÁ EDITANDO A FRUTA: <?= $row['nome'] ?> - <?= $row['raridade'] ?></h2><br>
-        <b>Nome:</b> <br><input type="text" name="new_fruit_name" value="<?= $row['nome'] ?>"><br><br>
-        <b>Raridade:</b> <br><input type="text" name="new_fruit_rarity" value="<?= $row['raridade'] ?>">
-        <input type="hidden" name="edit_id_fruta" value="<?= $row['id'] ?>">
-        <br><br>
-        <button style="padding: 10px; width: 200px; font-size: 20px;" type="submit" name="edited">EDIT</button>
-    </form>
+            ?></b></h2><br>
+        <div class="form">
+            <form action="../../Controller/form/tratamento.php" method="post">
+                <b>Nome:</b> <br><input class="dia" type="text" name="new_fruit_name" value="<?= $row['nome'] ?>"><br><br>
+                <b>Raridade:</b> <br><input class="dia" type="text" name="new_fruit_rarity" value="<?= $row['raridade'] ?>">
+                <input type="hidden" name="edit_id_fruta" value="<?= $row['id'] ?>">
+                <br><br>
+                <button class="enviar" type="submit" name="edited">EDIT</button>
+            </form>
+        </div>
+    </div>
 </div>
